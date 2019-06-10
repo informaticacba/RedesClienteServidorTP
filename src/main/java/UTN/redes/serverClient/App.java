109a2a7ee094f07e08aab963664f99055731a98d
Mauri05 <mauriagustin05@gmail.com>
2019-06-10 19:28:36 -0300

    first commit

---
+++ src/main/java/UTN/redes/serverClient/App.java
@@ -0,0 +1,13 @@
+package UTN.redes.serverClient;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
